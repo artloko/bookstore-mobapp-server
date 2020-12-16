e769d6792f22de6dadc3a17ee9b609801518fd77
artem1102zx <artem1102zx@gmail.com>
2020-12-17 02:08:36 +0300

    add requestbody annotation

--- src/main/java/org/bsu/famcs/bookstoremobappserver/controller/v1/ProfileController.java
+++ src/main/java/org/bsu/famcs/bookstoremobappserver/controller/v1/ProfileController.java
@@ -1,12 +1,11 @@
 package org.bsu.famcs.bookstoremobappserver.controller.v1;
 
-import org.bsu.famcs.bookstoremobappserver.controller.entity.SignUpUserRq;
 import org.bsu.famcs.bookstoremobappserver.controller.entity.UserInfoRs;
 import org.bsu.famcs.bookstoremobappserver.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,6 +20,7 @@ public class ProfileController {
     }
 
     @GetMapping("/user/info")
+    @ResponseBody
     public UserInfoRs signUp(@RequestParam String email) {
         return loginService.getUserInfo(email);
     }
