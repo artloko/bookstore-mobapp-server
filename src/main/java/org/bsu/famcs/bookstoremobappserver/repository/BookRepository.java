4913768d0c3ddfb941dcf48c994e61d1f48c2176
artem1102zx <artem1102zx@gmail.com>
2020-12-23 21:40:55 +0300

    fix get filtered books query

--- src/main/java/org/bsu/famcs/bookstoremobappserver/repository/BookRepository.java
+++ src/main/java/org/bsu/famcs/bookstoremobappserver/repository/BookRepository.java
@@ -8,8 +8,8 @@ import org.springframework.data.repository.query.Param;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
 
-    @Query("SELECT b FROM Book b inner join fetch Author as a inner join fetch Genre as g where b.name = :name " +
-            "and :author like a.name and :genre like g.name")
+    @Query("SELECT b FROM Book b inner join fetch Author as a inner join fetch Genre as g where (:name is null or b.name = :name) " +
+            "and (:autho is null or :author like a.name) and (:genre is null or :genre like g.name)")
     Iterable<Book> getFilteredBooks(@Param("name") String name, @Param("author") String author, @Param("genre") String genre);
 
 }
