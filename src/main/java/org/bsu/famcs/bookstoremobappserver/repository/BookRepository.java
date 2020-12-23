c08d200c96ea46b6c0943fd058be02d5f22cbf4f
artem1102zx <artem1102zx@gmail.com>
2020-12-23 21:41:39 +0300

    oh

--- src/main/java/org/bsu/famcs/bookstoremobappserver/repository/BookRepository.java
+++ src/main/java/org/bsu/famcs/bookstoremobappserver/repository/BookRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 public interface BookRepository extends CrudRepository<Book, Long> {
 
     @Query("SELECT b FROM Book b inner join fetch Author as a inner join fetch Genre as g where (:name is null or b.name = :name) " +
-            "and (:autho is null or :author like a.name) and (:genre is null or :genre like g.name)")
+            "and (:author is null or :author like a.name) and (:genre is null or :genre like g.name)")
     Iterable<Book> getFilteredBooks(@Param("name") String name, @Param("author") String author, @Param("genre") String genre);
 
 }
