87625e06c0a412f43a09c551bb79a51a5caca4a5
artem1102zx <artem1102zx@gmail.com>
2020-12-26 04:05:55 +0300

    world

--- src/main/java/org/bsu/famcs/bookstoremobappserver/controller/entity/OrdersRs.java
+++ src/main/java/org/bsu/famcs/bookstoremobappserver/controller/entity/OrdersRs.java
@@ -4,6 +4,9 @@ import org.bsu.famcs.bookstoremobappserver.controller.entity.to.BookTO;
 
 import java.util.List;
 
+@Data
+@RequiredArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class OrdersRs {
 
     private List<BookTO> books;
