1ff23ab1b419f989cd7bacd4322be506d529595c
artem1102zx <artem1102zx@gmail.com>
2020-12-26 04:08:39 +0300

    add forgotten imports

--- src/main/java/org/bsu/famcs/bookstoremobappserver/controller/entity/OrdersRs.java
+++ src/main/java/org/bsu/famcs/bookstoremobappserver/controller/entity/OrdersRs.java
@@ -1,5 +1,8 @@
 package org.bsu.famcs.bookstoremobappserver.controller.entity;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.bsu.famcs.bookstoremobappserver.controller.entity.to.BookTO;
 
 import java.util.List;
